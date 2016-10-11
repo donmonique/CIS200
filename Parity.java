7132d87c837c8b094a752473610463d0bb0bd712
donmonique <donmonique@live.com>
2016-10-11 10:53:54 -0500

    Update and rename Proj4.java to Parity.java

--- Parity.java
+++ Parity.java
@@ -8,7 +8,7 @@
 
 import java.io.*;
 import java.util.*;
-public class Proj4 {
+public class Parity {
 
 	public static void main(String[] args) throws IOException {
 		//Scanners and Initialization
